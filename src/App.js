e2925b432056ccbadca9ccd6ca74f6e05eaceeb4
Nikh <nikhil@faithconsultancy.in>
2019-08-10 18:10:37 +0530

    Started portfolio
    Added initial div and about div (started)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+const position = ['Node.js developer', 'PHP developer', 'React learner'];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IntroDiv></IntroDiv>
+      <AboutDiv></AboutDiv>
     </div>
   );
+}
+
+function IntroDiv() {
+  return (
+    <section id="Intro" className="App-header">
+    <div className="backgroundOverlay"></div>
+    <div className="Inner-text fadeIn">
+      <h1 className="Title">Hi, I'm Nikh!</h1>
+      <p className="fadeIn">I'm a </p><Position></Position>
+    </div>
+    <video autoPlay loop muted className="Background-video" src="water.mp4"></video>
+  </section>
+  )
+}
+
+function AboutDiv() {
+  return (
+    <section id="About_me" className="App-header">
+    <div className="Inner-text">
+      <h1 className="Title">About me</h1>
+    </div>
+  </section>
+  )
+}
+
+class Position extends React.Component {
+  constructor(props) {
+    super(props);
+    let randomPosition = Math.floor(Math.random() * (position.length));
+
+    this.state = {randomPosition: randomPosition};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      3000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    let randomPosition = Math.floor(Math.random() * (position.length));
+
+    this.setState({
+      randomPosition: randomPosition
+    });
+  }
+
+  render() {
+    return (
+      <p className="Subtitle">{position[this.state.randomPosition]}!</p>
+    );
+  }
 }
 
 export default App;
